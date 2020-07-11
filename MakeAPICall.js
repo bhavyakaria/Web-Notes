338a9f9e1c9057c2f677d3b5d2bbf029f2d9c75e
bhavyakaria <bhavyakaria@yahoo.in>
2020-07-11 15:42:20 +0530

    added axios code

--- MakeAPICall.js
+++ MakeAPICall.js
@@ -1,8 +1,18 @@
+// using inbuilt http request
 const http = require('https');
-const { Http2ServerRequest } = require('http2');
+const axios = require('axios');
 
 http.get('https://jsonplaceholder.typicode.com/todos/1', (res) => {
     console.log(res);
 }).on('error', (err) => {
     console.log(error);
 });
+
+// using axios libray by installing axios npm package
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => {
+        console.log(res);
+    })
+    .catch(err => {
+        console.log(err);
+    });
